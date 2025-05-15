45093d33f80b1d01ca24ef8b4bf5502978d8ee40
maxamerv <maxamerv@gmail.com>
2025-05-15 15:46:41 +0200

    Merge?

---
+++ Hello.py
@@ -0,0 +1,14 @@
+def compute_prime_factors(n):
+    """Compute the prime factors of a given number n."""
+    i = 2
+    factors = []
+    while i * i <= n:
+        print(f"Current i: {i}, Current n: {n}") # This is Max's debug step
+        if n % i:
+            i += 1
+        else:
+            n //= i
+            factors.append(i)
+    if n > 1:
+        factors.append(n)
+    return factors
